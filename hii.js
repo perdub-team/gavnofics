96d5c73f3675a1d8ca7bf3828f186ee8b087a3c1
perdub <81479253+lesh6295-png@users.noreply.github.com>
2023-04-14 19:56:59 +0300

    Add files via upload

---
+++ hii.js
@@ -0,0 +1,3 @@
+let hellos = ["повар что ты тут забыл? ты не будешь здесь спамить","купи уа","АААААА ГОВНО ТЕЧЁТ ПО ЛЯЖКАМ?","https://vk.com/wall-22751485_4100193","что ты тут делаешь???"]
+
+console.log(hellos[Math.floor((Math.random()*hellos.length))]);
